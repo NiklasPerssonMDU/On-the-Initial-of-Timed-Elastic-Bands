8db996e85428ada73c95af8ab149605c9f81e479
NiklasPerssonMDU <100279902+NiklasPerssonMDU@users.noreply.github.com>
2022-11-23 10:39:01 +0100

    Delete IntSdH.m

--- IntSdH.m
+++
@@ -1,8 +0,0 @@
-function [IAdH, ISdH] = IntSdH(heading)
-        Ts=0.1;
-        %we compute both the Integral Squared heading derivative and...
-        ISdH = sum( ( atan2(sin(diff(heading)), cos(diff(heading)) )  ./Ts).^2 );
-
-        %the Integral of Absolute heading derivative
-        IAdH = sum( abs(( atan2(sin(diff(heading)), cos(diff(heading)) )./Ts)) );
-end
